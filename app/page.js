cb7a287578286e46fdea86251161137589d889c0
Johnnam1121 <johnnam93@gmail.com>
2024-08-01 19:21:48 -0500

    folder naming issues

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import mainPic from '@assets/HomepageImages/LandingPhoto.jpg'
+import mainPic from '@assets/LandingPageImages/LandingPhoto.jpg'
 
 export default function Home() {
   return (
