049e28beed465709b39452ff6b25b119dfa5ac90
Johnnam1121 <johnnam93@gmail.com>
2024-08-01 19:19:03 -0500

    fixed some warnings and vercel deployment issues

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,33 +5,33 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <div>
-      <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
-        <div class="mx-auto max-w-screen-xl text-center">
-          <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
+      <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+        <div className="mx-auto max-w-screen-xl text-center">
+          <a href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
             <Image src={logo} height={300} width={300}/>
           </a>
-          <p class="my-6 text-gray-500 dark:text-gray-400">Be part of something bigger in the Houston community! </p>
-          <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+          <p className="my-6 text-gray-500 dark:text-gray-400">Be part of something bigger in the Houston community! </p>
+          <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
             <li>
-              <a href="/MeetOurTeam" class="mr-4 hover:underline md:mr-6 ">Meet Our Team</a>
+              <a href="/MeetOurTeam" className="mr-4 hover:underline md:mr-6 ">Meet Our Team</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">KASH History</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">KASH History</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">Upcoming Events</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Upcoming Events</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">Contact Us</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Contact Us</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">FAQs</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6">FAQs</a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">Become a Member</a>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Become a Member</a>
             </li>
           </ul>
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">KASH fhdsklaflsda</a>. All Rights Reserved.</span>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">KASH fhdsklaflsda</a>. All Rights Reserved.</span>
         </div>
       </footer>
     </div>
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,33 +5,33 @@ import Image from 'next/image'
 export default function Navbar() {
   return (
     <div>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="flex flex-wrap items-center justify-between lg:mx-16 p-4">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap items-center justify-between lg:mx-16 p-4">
           <a href="/">
             <Image src={logo} width={100} height={100} />
           </a>
-          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a href="/MeetOurTeam" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Meet Our Team</a>
+                <a href="/MeetOurTeam" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Meet Our Team</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">KASH History</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">KASH History</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Upcoming Events</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Upcoming Events</a>
               </li>
               <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
               </li>
               <li>
-                <a href="https://app.joinit.com/o/kash" target="blank" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Become a Member</a>
+                <a href="https://app.joinit.com/o/kash" target="blank" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Become a Member</a>
               </li>
             </ul>
           </div>
