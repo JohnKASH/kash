2eeae6253f7355801419b7b16a7133abd8bf7cc1
Johnnam1121 <johnnam93@gmail.com>
2025-06-01 23:15:12 -0500

    testing img import

--- app/MeetOurTeam/page.js
+++ app/MeetOurTeam/page.js
@@ -1,42 +1,42 @@
-// import TeamMember from "app/MeetOurTeam/MeetOurTeam/TeamMember";
-// import Image from "next/image";
-// import BennySideEye from '@assets/MeetOurTeamImages/BennySideEye.jpeg'
+import TeamMember from "app/MeetOurTeam/MeetOurTeam/TeamMember";
+import Image from "next/image";
+import BennySideEye from '@assets/MeetOurTeamImages/BennySideEye.jpeg'
 
-// export default function Home() {
-//   return (
-//     <div className="">
-//       <div>
-//         <section className="bg-white">
-//           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-//             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-//               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Our Team</h2>
-//               <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">Info about team lorem ipsum Info about team lorem ipsum Info about team lorem ipsum Info about team lorem ipsum</p>
-//             </div>
-//             <div className="sm:grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//             </div>
-//           </div>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="bg-white">
-//           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-//             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-//               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Our Board Members</h2>
-//               <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">Information about board lorem ipsumInformation about board lorem ipsum Information about board lorem ipsum</p>
-//             </div>
-//             <div className="sm:grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//               <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
-//             </div>
-//           </div>
-//         </section>
-//       </div>
-//     </div>
-//   );
-// }
+export default function Home() {
+  return (
+    <div className="">
+      <div>
+        <section className="bg-white">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+            <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Our Team</h2>
+              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">Info about team lorem ipsum Info about team lorem ipsum Info about team lorem ipsum Info about team lorem ipsum</p>
+            </div>
+            <div className="sm:grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+            </div>
+          </div>
+        </section>
+      </div>
+      <div>
+        <section className="bg-white">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+            <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Our Board Members</h2>
+              <p className="font-light text-gray-500 lg:mb-16 sm:text-xl ">Information about board lorem ipsumInformation about board lorem ipsum Information about board lorem ipsum</p>
+            </div>
+            <div className="sm:grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+              <TeamMember name='john' title='webdev' description='builds websites builds websites builds websites' image={BennySideEye.src} />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -37,9 +37,9 @@ export default function Navbar() {
               <li className={`${isDropdownOpen ? 'underline' : ''}`}>
                 <a href="/MentorshipProgram" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-kashBlue-600 md:p-0">2025 Mentorship Program</a>
               </li>
-              {/* <li className={`${isDropdownOpen ? 'underline' : ''}`}>
+              <li className={`${isDropdownOpen ? 'underline' : ''}`}>
                 <a href="/MeetOurTeam" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-kashBlue-600 md:p-0">Meet Our Team</a>
-              </li> */}
+              </li>
               {/* <li className={`${isDropdownOpen ? 'underline' : ''}`}>
                 <a href="/KASHHistory" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-kashBlue-600 md:p-0">KASH History</a>
               </li> */}
